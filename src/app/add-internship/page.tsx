c885b2512ebccf2c15fa97b1621e118cf5bcf1b4
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-01-11 13:57:47 +0100

    feat: WIP internship form

---
+++ src/app/add-internship/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import AddInternshipForm from '@/components/add-internship-form';
+
+export default function AddInternshipPage() {
+	return (
+		<>
+			<h1>Add an internship</h1>
+			<AddInternshipForm />
+		</>
+	);
+}
