64e96f8446ecf0ce0a8c98086b5cc9d5024522a5
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-02-04 16:50:27 +0100

    feat: add data is not found msg in internships table

--- src/components/internships-table.tsx
+++ src/components/internships-table.tsx
@@ -1,69 +1,86 @@
 'use client';
-import React, { useState } from 'react';
-import {IconButton, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import { MdFileUpload } from 'react-icons/md';
-import FileUpload from './upload-file-form';
 import { InternshipsTableProps } from '@/api/internship/get-internships';
 import { DateUtil } from '@/utils/date.util';
+import { IconButton, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { useState } from 'react';
+import { MdFileUpload } from 'react-icons/md';
+import FileUpload from './upload-file-form';
 
-const InternshipsTable = ({ data }: { data: InternshipsTableProps[]}) => {
+const InternshipsTable = ({ data }: { data: InternshipsTableProps[] }) => {
+	const [isUploadPopupOpen, setUploadPopupOpen] = useState(false);
 
-  const [isUploadPopupOpen, setUploadPopupOpen] = useState(false);
+	const openUploadPopup = () => {
+		setUploadPopupOpen(true);
+	};
 
-  const openUploadPopup = () => {
-    setUploadPopupOpen(true);
-  };
+	const closeUploadPopup = () => {
+		setUploadPopupOpen(false);
+	};
 
-  const closeUploadPopup = () => {
-    setUploadPopupOpen(false);
-  };
+	const handleUpload = (file) => {
+		// Implement logic to handle the file upload
+		console.log('Uploading file:', file);
+	};
 
-  const handleUpload = (file) => {
-    // Implement logic to handle the file upload
-    console.log('Uploading file:', file);
-  };
+	return (
+		<Table size="lg">
+			<Thead>
+				<Tr>
+					<Th style={{ textAlign: 'center' }}>Internship ID</Th>
+					<Th style={{ textAlign: 'center' }}>Starting Date</Th>
+					<Th style={{ textAlign: 'center' }}>End Date</Th>
+					<Th style={{ textAlign: 'center' }}>Duration</Th>
+					<Th style={{ textAlign: 'center' }}>Title</Th>
+					<Th style={{ textAlign: 'center' }}>Status</Th>
+					<Th style={{ textAlign: 'center' }}>Documents</Th>
+				</Tr>
+			</Thead>
+			<Tbody>
+				{data && data.length > 0 ? (
+					data.map((internship) => (
+						<Tr key={internship.id}>
+							<Td style={{ textAlign: 'center' }}>{internship.id}</Td>
+							<Td style={{ textAlign: 'center' }}>
+								{DateUtil.formatUSDate(internship.startDate)}
+							</Td>
+							<Td style={{ textAlign: 'center' }}>
+								{DateUtil.formatUSDate(internship.endDate)}
+							</Td>
+							<Td style={{ textAlign: 'center' }}>
+								{DateUtil.diffInDays(
+									new Date(internship.startDate),
+									new Date(internship.endDate)
+								)}{' '}
+								Days
+							</Td>
+							<Td style={{ textAlign: 'center' }}>{internship.title}</Td>
+							<Td style={{ textAlign: 'center' }}>{internship.status}</Td>
+							<Td style={{ textAlign: 'center' }}>
+								<IconButton
+									aria-label="expand row"
+									fontSize="2xl"
+									icon={<MdFileUpload />}
+									onClick={openUploadPopup}
+								></IconButton>
 
-  return (
-      <Table size='lg'>
-        <Thead>
-          <Tr>
-            <Th  style={{ textAlign: "center" }}>Internship ID</Th>
-            <Th  style={{ textAlign: "center" }}>Starting Date</Th>
-            <Th  style={{ textAlign: "center" }}>End Date</Th>
-            <Th  style={{ textAlign: "center" }}>Duration</Th>
-            <Th  style={{ textAlign: "center" }}>Title</Th>
-            <Th  style={{ textAlign: "center" }}>Status</Th>
-            <Th  style={{ textAlign: "center" }}>Documents</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-         {data.map(internship =>
-          <Tr key={internship.id}>
-            <Td style={{ textAlign: "center" }}>{internship.id}</Td>
-            <Td style={{ textAlign: "center" }}>{DateUtil.formatUSDate(internship.startDate)}</Td>
-            <Td style={{ textAlign: "center" }}>{DateUtil.formatUSDate(internship.endDate)}</Td>
-            <Td style={{ textAlign: "center" }}>{DateUtil.diffInDays(new Date(internship.startDate),new Date(internship.endDate))} Days</Td>
-            <Td style={{ textAlign: "center" }}>{internship.title}</Td>
-            <Td style={{ textAlign: "center" }}>{internship.status}</Td>
-            <Td style={{ textAlign: "center" }}>
-              <IconButton
-              aria-label="expand row"
-              fontSize='2xl'
-              icon={<MdFileUpload/>}
-              onClick={openUploadPopup}>
-              </IconButton>
-
-              <FileUpload
-              isOpen={isUploadPopupOpen}
-              onClose={closeUploadPopup}
-              onUpload={handleUpload}
-               />
-            </Td>
-          </Tr>
-        )}
-        </Tbody>
-      </Table>
-  );
+								<FileUpload
+									isOpen={isUploadPopupOpen}
+									onClose={closeUploadPopup}
+									onUpload={handleUpload}
+								/>
+							</Td>
+						</Tr>
+					))
+				) : (
+					<Tr>
+						<Td colSpan={7} textAlign="center">
+							<p>No data is found.</p>
+						</Td>
+					</Tr>
+				)}
+			</Tbody>
+		</Table>
+	);
 };
 
 export default InternshipsTable;
