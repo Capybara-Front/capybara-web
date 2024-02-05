40359a800e36524a626d0b01cd2954e023b72b30
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-02-05 22:56:16 +0100

    feat: back button

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,10 +1,14 @@
 import { AddInternShipForm } from '@/components/add-internship-form';
-import { Container, Heading } from '@chakra-ui/react';
+import { Container, Heading, Link } from '@chakra-ui/react';
+import { MdArrowBack } from 'react-icons/md';
 
 export default function AddInternshipPage() {
 	return (
 		<Container maxWidth="5xl" pt={16}>
-			<Heading size="xl" mb={10}>
+			<Heading size="xl" mb={10} display="flex" alignItems="center" gap={2}>
+				<Link href="/">
+					<MdArrowBack />
+				</Link>
 				Add Internship
 			</Heading>
 			<AddInternShipForm />
