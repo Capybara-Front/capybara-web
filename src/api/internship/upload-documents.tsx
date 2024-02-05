28c39c1c5d1ed197352e9ca36207a565c8a9a308
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-02-05 23:15:11 +0100

    fix: missing return

--- src/api/internship/upload-documents.tsx
+++ src/api/internship/upload-documents.tsx
@@ -1,33 +1,35 @@
 type FileMetadata = {
-    filename: string;
-    levelOfConfidentiality: number;
-}
+	filename: string;
+	levelOfConfidentiality: number;
+};
 
 export async function uploadDocuments(internshipID: any, files: File[]) {
-    try {
+	try {
+		const formData = new FormData();
+		const metadata: FileMetadata[] = [];
 
-        const formData = new FormData();
-        const metadata: FileMetadata[] = [];
+		files.forEach((file) => {
+			metadata.push({ filename: file.name, levelOfConfidentiality: 1 });
+			formData.append('files', file);
+		});
 
-        files.forEach((file) => {
-            metadata.push({ filename: file.name, levelOfConfidentiality: 1 });
-            formData.append('files', file);
-        });
+		formData.append('data', JSON.stringify({ metadata }));
 
-        formData.append('data', JSON.stringify({ metadata }));
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_API_URL}/documents/internships/${internshipID}`,
+			{
+				method: 'POST',
+				body: formData,
+			}
+		);
 
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/documents/internships/${internshipID}`, {
-            method: 'POST',
-            body: formData,
-        });
+		if (!res.ok) {
+			throw new Error('Failed to upload documents');
+		}
 
-        if (!res.ok) {
-            throw new Error('Failed to upload documents');
-        }
-
-        await res.json();
-    } catch (err) {
-        console.error('Unable to upload documents', err);
-        Promise.reject(new Error('Unable to upload Documents.'));
-    }
+		await res.json();
+	} catch (err) {
+		console.error('Unable to upload documents', err);
+		return Promise.reject(new Error('Unable to upload Documents.'));
+	}
 }
