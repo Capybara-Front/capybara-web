a3a120988efbbb4328b187687343f34bfd4eda38
daniel <daniel.aguiar@outlook.fr>
2024-02-05 19:04:54 +0100

    feat: upload documents

--- src/api/internship/upload-documents.tsx
+++ src/api/internship/upload-documents.tsx
@@ -1,34 +1,33 @@
-export async function uploadDocument(internshipID: any, files : File[]) {
-	try {
+type FileMetadata = {
+    filename: string;
+    levelOfConfidentiality: number;
+}
+
+export async function uploadDocuments(internshipID: any, files: File[]) {
+    try {
 
         const formData = new FormData();
-        const metadata = [];
+        const metadata: FileMetadata[] = [];
 
-        for (let i = 0; i<files.length; i++){
-            formData.append('files', files[i]);
-            metadata.push({filename:files[i].name,levelOfConfidentiality:1})
-        };
+        files.forEach((file) => {
+            metadata.push({ filename: file.name, levelOfConfidentiality: 1 });
+            formData.append('files', file);
+        });
 
-        formData.append('data',JSON.stringify({metadata}));
+        formData.append('data', JSON.stringify({ metadata }));
 
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/documents/internships/${internshipID}`, {
-			method: 'POST',
-			body: formData,
-		});
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/documents/internships/${internshipID}`, {
+            method: 'POST',
+            body: formData,
+        });
 
-        if (!res.ok){
-            throw new Error('Failed to upload document');
+        if (!res.ok) {
+            throw new Error('Failed to upload documents');
         }
 
-        try {
-            const jsonResponse = await res.json();
-            console.log('Res : ', jsonResponse);
-        } catch (jsonError) {
-            console.log('Error parsing JSON: ', jsonError);
-        }
-
-	} catch (err) {
-        console.log('----RES failed', err);
-		Promise.reject(new Error('Unable to upload Documents.'));
-	}
+        await res.json();
+    } catch (err) {
+        console.error('Unable to upload documents', err);
+        Promise.reject(new Error('Unable to upload Documents.'));
+    }
 }
